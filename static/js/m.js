20666cfdf1966b8ae342098fc5d38856e09f25da
young <younger@email.com>
2018-10-08 07:02:07 +0800

    m

--- static/js/m.js
+++ static/js/m.js
@@ -1,47 +1,30 @@
 
-
-
 $(document).ready(function () {
 	$(".navbar-nav>li>a").click(function () {
 		$(".m-current-item").removeClass("m-current-item");
 		$(this).addClass("m-current-item");
+	}).mouseenter(function () {
+		$(this).addClass("m-enter-item"); 
 
 		crtid = $(this).attr("id");
 		if (crtid == "aboutus") {
-			megaDislayFlag = $("#mega").is(":visible");
-			megaAboutusFlag = $("#mega-aboutus").is(":visible");
-
-			//alert("mega: " + megaDislayFlag + ", megaAboutusFlag: " + megaAboutusFlag); 
-			if (megaDislayFlag && megaAboutusFlag) {
-				$("#mega").slideUp("slow");;
-			} else {
-				$("#mega-service").hide();
-				$("#mega-aboutus").show();
-				$("#mega").slideDown("slow");
-			}
+			$("#mega-service").hide();
+			$("#mega-aboutus").show();
+			$("#mega").show();
 		} else if (crtid == "service") {
-			megaDislayFlag = $("#mega").is(":visible");
-			megaServiceFlag = $("#mega-service").is(":visible");
-			//alert("mega: " + megaDislayFlag + ", megaServiceFlag: " + megaServiceFlag); 
-			if (megaDislayFlag && megaServiceFlag) {
-				$("#mega").slideUp("slow");
-			} else {
-				$("#mega-aboutus").hide();
-				$("#mega-service").show();
-				$("#mega").slideDown("slow");
-			}
-		};
-
-	}).mouseenter(function () {
-		$(this).addClass("m-enter-item");
+			$("#mega-service").show();
+			$("#mega-aboutus").hide();
+			$("#mega").show();
+		} else {
+			$("#mega").hide();
+		}
 	}).mouseleave(function () {
-		$(this).removeClass("m-enter-item");
-	});
+		$(this).removeClass("m-enter-item"); 
+	}); 
 
-	// 鼠标滚动时, 隐藏mega
-	$("window").scroll(function () {
-		console.log("33333333333333333")
-		$("#mega").hide();
+	// when mouse leave #mega. hide(); 
+	$("#mega").mouseleave(function() {
+		$("#mega").hide(); 
 	});
 
 });
